14eb375c13b4df326e94a6753abc655abf803493
jalol <jaloliddin9917@gmail.com>
2023-04-18 00:41:21 +0500

    build front pro

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 // Routes
@@ -9,7 +10,14 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
